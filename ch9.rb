a7a7d145beb4cb4cfaada8eae2778151d90abe5e
Russ Robbins <russ.robbins@gmail.com>
2014-04-10 11:08:03 -0400

    Rest of Precourse code

--- ch9.rb
+++ ch9.rb
@@ -39,7 +39,34 @@
 
 
 # Old school Roman Numerals
-def old_roman_numeral num
+# def old_roman_numeral num
+#   keys = [
+#     [1000, "M"],
+#     [500, "D"],
+#     [100, "C"],
+#     [50, "L"],
+#     [10, "X"],
+#     [5, "V"],
+#     [1, "I"],
+#   ]
+
+#   ret = ""
+#   keys.each do |pair|
+#     numeral = pair[0]
+#     letter = pair[1]
+#     while (num - numeral) >= 0
+#       ret += letter
+#       num = num - numeral
+#     end
+#   end
+#   return ret
+# end
+
+# puts old_roman_numeral 1299
+
+
+# New school Roman Numerals
+def new_roman_numeral num
   keys = [
     [1000, "M"],
     [500, "D"],
@@ -49,23 +76,33 @@ def old_roman_numeral num
     [5, "V"],
     [1, "I"],
   ]
-  ret = ""
 
-  #while num >= 0
-    keys.each do |pair|
-      divisor = pair[0]
-      letter = pair[1]
-      quotient = num/divisor
-      if quotient > 0
-        ret += letter
+  ret = ""
+  keys.each_with_index do |pair, index|
+    numeral = pair[0]
+    letter = pair[1]
+    if index>0
+      last_numeral = keys[(index-1)][0]
+      last_letter = keys[(index-1)][1]
+    else
+      last_numeral = 1
+      last_letter = ""
+    end
+    while (num - numeral) >= 0
+      comp = num - (num % numeral)
+      if (comp + numeral) == last_numeral
+        ret += letter + last_letter
+        num = num - (last_numeral - numeral)
       else
-        #break
+        ret += letter
+        num = num - numeral
       end
-      num = num%divisor
     end
   end
   return ret
-#end
+end
 
-puts old_roman_numeral 2010
+puts "Enter a decimal number under 3000:"
+target = gets.chomp
+puts new_roman_numeral target.to_i
 
